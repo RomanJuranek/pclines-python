c407eb42e1f9d7cdd7e65500ee6d0dd5ef9ae438
Roman Juranek <ijuranek@fit.vutbr.cz>
2020-08-03 23:43:15 +0200

    fixed accumulation, added line param calc

--- pclines.py
+++ pclines.py
@@ -58,9 +58,9 @@ def accumulate(x, bbox=None, d=256):
         ofs = (d-ranges) / 2
         (x0,y0),(x1,y1) = ofs, (ranges-1)+ofs
         print(bbox)
-        norm0 = Normalizer((y,y+h), (y0, y1))
+        norm0 = Normalizer((y,y+h), (y1, y0))
         norm1 = Normalizer((x,x+w), (x0, x1))
-        norm2 = Normalizer((y,y+h), (y1, y0))
+        norm2 = Normalizer((y,y+h), (y0, y1))
         return norm0, norm1, norm2
 
     norm0, norm1, norm2 = normalizers()
@@ -71,7 +71,7 @@ def accumulate(x, bbox=None, d=256):
         norm2(x[:,1])
     ]
 
-    for a,b,c in zip(*x):
+    for a,b,c in zip(*x):  # remove space wraping
         t_part = np.linspace(a,b,d)
         s_part = np.linspace(b,c,d)
         c = np.arange(2*d-1,dtype="i")
@@ -82,19 +82,28 @@ def accumulate(x, bbox=None, d=256):
     return A
 
 
-def lines_parameters(shape, d, peaks):
-    P = [u,v] 
+def lines_parameters(peaks, d, bbox):
+    u = peaks[:,1]
+    v = peaks[:,0]
+
     #centrovanie
     u = u - (d - 1)
+
+    x,y,w,h = bbox
+    shape = w,h
     m = max(shape) - 1
     normV = Normalizer((0,d-1),(-m/2, m/2))
-    v = normV.transform(v)
+    v = normV(v)
 
-    if (u < d):
-        l = [d-u, u, -v*d]
-    else:
-        l = [d+u, u, -v*d]
+    f = u < 0
+
+    l = np.array([f*(d+u)+(1-f)*(d-u), u, -v*d], "f").T
+
+    tx,ty = x+0.5*w, y+0.5*h
+
+    l[:,2] -= l[:,0]*tx + l[:,1]*ty 
     
+    return l
     
 
 
@@ -115,9 +124,9 @@ def lines_parameters(shape, d, peaks):
 
 def find_peaks(A, t):
     prominence = dilation(A+1)/erosion(A+1)
-    peaks = peak_local_max(A, threshold_abs=t)
-    c,r = peaks[:,0], peaks[:,1]
+    peaks = peak_local_max(A, threshold_abs=t, min_distance=1)
+    r,c = peaks[:,0], peaks[:,1]
     value = A[r,c]
-    valid = prominence[r,c] > 1.5
+    valid = prominence[r,c] > 1.3
     return peaks[valid], value[valid]
     
